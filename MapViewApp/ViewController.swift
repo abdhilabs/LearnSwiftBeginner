35f93a1ba996a3af85e0364494ee29dda55a8299
Abdhilabs <rizaabdhi@gmail.com>
2020-04-21 06:48:30 +0700

    done map view

--- MapViewApp/ViewController.swift
+++ MapViewApp/ViewController.swift
@@ -7,12 +7,31 @@
 //
 
 import UIKit
+import MapKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var mapView: MKMapView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+    
+        //1
+        mapView.mapType = .standard
+        //2
+        let location = CLLocationCoordinate2D(latitude: -7.370380, longitude: 110.314290)
+        //3
+        let span = MKCoordinateSpan(latitudeDelta: 0.05, longitudeDelta: 0.05)
+        let region = MKCoordinateRegion(center: location, span: span)
+        mapView.setRegion(region, animated: true)
+        
+        //4
+        let annotation = MKPointAnnotation()
+        annotation.coordinate = location
+        annotation.title = "Magelang"
+        annotation.subtitle = "Grabag"
+        mapView.addAnnotation(annotation)
+        
     }
 
 
