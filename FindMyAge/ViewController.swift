e7c56f27ec8082807b77ea360f46fea4676d1c8d
Abdhilabs <rizaabdhi@gmail.com>
2020-04-20 14:26:48 +0700

    First Commit and Done

--- FindMyAge/ViewController.swift
+++ FindMyAge/ViewController.swift
@@ -10,11 +10,36 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var datePicker: UIDatePicker!
+    @IBOutlet weak var ageLabel: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func calculateMyAge(_ sender: Any) {
+        //Get selected date from date picker
+        let birthDate = self.datePicker.date //(yyyy-mm-dd hh:mm:ss)
+        
+        let today = Date()
+        
+        if birthDate >= today {
+            let alerController = UIAlertController(title: "Error", message: "Please enter a valid date", preferredStyle: .alert)
+            let alertAction = UIAlertAction(title: "Okay", style: .default, handler: nil)
+            alerController.addAction(alertAction)
+            self.present(alerController, animated: true, completion: nil)
+            return
+        }
+        let calendar = Calendar.current
+        
+        let component = calendar.dateComponents([.year,.month,.day], from: birthDate, to: today)
+        
+        guard let ageYears = component.year else {return}
+        guard let ageMonth = component.month else {return}
+        guard let ageDay = component.day else {return}
+        
+        self.ageLabel.text = "\(ageYears) years, \(ageMonth) month, \(ageDay) day"
+    }
+    
 }
 
