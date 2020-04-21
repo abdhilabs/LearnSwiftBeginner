1337a985e6fa32647c8c8a6d158aa9329c5ee449
Abdhilabs <rizaabdhi@gmail.com>
2020-04-21 10:33:40 +0700

    Initial Commit

---
+++ Data Pegawai/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Data Pegawai
+//
+//  Created by Abdhi on 21/04/20.
+//  Copyright © 2020 Abdhilabs. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
